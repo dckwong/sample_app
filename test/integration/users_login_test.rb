332b6428d5ae7177b1e281212510ee3a6f38491c
Derek <derek_kwong8@yahoo.com>
2016-12-30 02:14:43 -0800

    Implemented advanced login

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -5,6 +5,17 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
 		@user = users(:michael)
 	end
 
+	test "login with remembering" do
+		log_in_as(@user, remember_me: '1')
+		assert_equal cookies['remember_token'], assigns(:user).remember_token
+	end
+
+	test "login without remembering" do
+		log_in_as(@user, remember_me: '1')
+		log_in_as(@user, remember_me: '0')
+		assert_empty cookies['remember_token']
+	end
+
 	test "login with invalid information" do
 		get login_path
 		assert_template 'sessions/new'
@@ -29,10 +40,15 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
    		delete logout_path
    		assert_not is_logged_in?
    		assert_redirected_to root_url
+   		delete logout_path
    		follow_redirect!
    		assert_select "a[href=?]", login_path
    		assert_select "a[href=?]", logout_path, count: 0
    		assert_select "a[href=?]", user_path(@user), count:0
+	end
+
+	test "authenticated? should return false for a user with a nil digest" do
+		assert_not @user.authenticated?('')
 	end
 
 end
